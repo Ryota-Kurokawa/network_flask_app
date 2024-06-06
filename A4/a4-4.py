ba0009b8d0f0d5680c78483b28630404861c3eed
ryota <kurokawa.ryota.1124@gmail.com>
2024-06-06 11:59:43 +0900

    4

---
+++ A4/a4-4.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def fileupload():
+    return render_template("a4-4.html")
+
+
+@app.route("/", methods=["POST"])
+def filewrite():
+    message = ""
+    event_date = request.form["event_date"]
+    event_name = request.form["event_name"]
+    try:
+        datetime.strptime(event_date, "%Y-%m-%d")
+        with open("a4-3.txt", "a", encoding="utf-8") as file:
+            file.write(f"\n{event_date},{event_name}")
+        message = "done"
+    except ValueError:
+        message = "error"
+    return render_template("a4-4.html", message=message)
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host="localhost")
